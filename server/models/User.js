e206a163cdfd1400ffd5c0b89efa00371da891cb
Lakshkyada <lakshkyada22@gmail.com>
2025-09-22 23:52:16 +0530

    userSchema full_naem->full_name

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
     _id: {type:String, required:true},
     email: {type:String, required:true},
-    full_naem: {type:String, required:true},
+    full_name: {type:String, required:true},
     username: {type:String, unique:true},
     bio: {type:String, default:'Hey there! I am using PingUp.'},
     profile_picture: {type:String, default:''},
