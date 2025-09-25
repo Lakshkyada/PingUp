5e424962db98e2e9d6e635f7fe806c6004a7933b
Lakshkyada <lakshkyada22@gmail.com>
2025-09-25 23:15:58 +0530

    again for email added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,21 @@ import Profile from './pages/Profile'
 import CreatePost from './pages/CreatePost'
 import Discover from './pages/Discover'
 import Connections from './pages/Connections'
-import { useClerk } from "@clerk/clerk-react"
-import {useUser} from '@clerk/clerk-react'
+
+import {useUser, useAuth} from '@clerk/clerk-react'
 import Layout from './pages/Layout'
 import {Toaster} from 'react-hot-toast'
+import { useEffect } from 'react'
 const App = () => {
   const {user} = useUser()
+  const {getToken} = useAuth()
+
+  useEffect(()=>{
+     if(user){
+       getToken().then((token)=>console.log(token))
+     }
+  },[user])
+
   return (
     <>      
         <Toaster/> 
